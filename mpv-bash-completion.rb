e8a4ff3a40011856fc7e6cd49d9a974efa9b2a0c
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-02-02 16:09:33 +0700

    more audit fixes

--- mpv-bash-completion.rb
+++ mpv-bash-completion.rb
@@ -4,9 +4,9 @@ class MpvBashCompletion < Formula
   head "https://github.com/2ion/mpv-bash-completion.git"
 
   depends_on "deus0ww/tap/mpv"
-  depends_on "lua"
   depends_on "bash"
   depends_on "coreutils"
+  depends_on "lua"
 
   def install
     system "make", "-f", "Makefile.osx"
