e815229d3d1ea7a6206dccd09b18a3fd1c09a8e7
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-02-06 01:43:31 +0700

    Updated ffmpeg deps from homebrew core + openssl + fdk-aac

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -7,15 +7,10 @@ class Ffmpeg < Formula
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
-  option "with-fdk-aac", "Enable the Fraunhofer FDK AAC library"
   option "with-librsvg", "Enable SVG files as inputs via librsvg"
   option "with-libssh", "Enable SFTP protocol via libssh"
-  option "with-tesseract", "Enable the tesseract OCR engine"
   option "with-libvidstab", "Enable vid.stab support for video stabilization"
   option "with-openh264", "Enable OpenH264 library"
-  option "with-openjpeg", "Enable JPEG 2000 image format"
-  option "with-openssl", "Enable SSL support"
-  option "with-rubberband", "Enable rubberband library"
   option "with-webp", "Enable using libwebp to encode WEBP images"
   option "with-zeromq", "Enable using libzeromq to receive commands sent through a libzeromq client"
   option "with-zimg", "Enable z.lib zimg library"
@@ -28,6 +23,7 @@ class Ffmpeg < Formula
 
   depends_on "deus0ww/tap/aom"
   depends_on "deus0ww/tap/libass"
+  depends_on "fdk-aac"
   depends_on "fontconfig"
   depends_on "freetype"
   depends_on "frei0r"
@@ -36,11 +32,15 @@ class Ffmpeg < Formula
   depends_on "libvorbis"
   depends_on "libvpx"
   depends_on "opencore-amr"
+  depends_on "openjpeg"
+  depends_on "openssl"
   depends_on "opus"
   depends_on "rtmpdump"
+  depends_on "rubberband"
   depends_on "sdl2"
   depends_on "snappy"
   depends_on "speex"
+  depends_on "tesseract"
   depends_on "theora"
   depends_on "x264"
   depends_on "x265"
@@ -48,7 +48,6 @@ class Ffmpeg < Formula
   depends_on "xz"
 
   depends_on "chromaprint" => :optional
-  depends_on "fdk-aac" => :optional
   depends_on "fontconfig" => :optional
   depends_on "game-music-emu" => :optional
   depends_on "libbluray" => :optional
@@ -61,11 +60,7 @@ class Ffmpeg < Formula
   depends_on "libvidstab" => :optional
   depends_on "libvmaf" => :optional
   depends_on "openh264" => :optional
-  depends_on "openjpeg" => :optional
-  depends_on "openssl" => :optional
-  depends_on "rubberband" => :optional
   depends_on "srt" => :optional
-  depends_on "tesseract" => :optional
   depends_on "two-lame" => :optional
   depends_on "wavpack" => :optional
   depends_on "webp" => :optional
@@ -85,10 +80,24 @@ class Ffmpeg < Formula
       --host-ldflags=#{ENV.ldflags}
       --enable-ffplay
       --enable-gpl
+      --enable-nonfree
+      --enable-opencl
+      
+      --enable-frei0r
       --enable-libaom
+      --enable-libass
+      --enable-libfdk-aac
+      --enable-libfontconfig
+      --enable-libfreetype
       --enable-libmp3lame
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-libopenjpeg
       --enable-libopus
+      --enable-librtmp
       --enable-libsnappy
+      --enable-libsoxr
+      --enable-libspeex
       --enable-libtheora
       --enable-libvorbis
       --enable-libvpx
@@ -96,34 +105,29 @@ class Ffmpeg < Formula
       --enable-libx265
       --enable-libxvid
       --enable-lzma
-      --enable-libfontconfig
-      --enable-libfreetype
-      --enable-frei0r
-      --enable-libass
-      --enable-libopencore-amrnb
-      --enable-libopencore-amrwb
-      --enable-librtmp
-      --enable-libspeex
-      --enable-libsoxr
-      --enable-videotoolbox
+      --enable-openssl
+      --disable-decoder=jpeg2000
       --disable-libjack
       --disable-indev=jack
     ]
+
+    if OS.mac?
+      args << "--enable-videotoolbox"
+    elsif OS.linux?
+      args << "--disable-videotoolbox"
+    end
 
     args << "--enable-chromaprint" if build.with? "chromaprint"
     args << "--enable-libbluray" if build.with? "libbluray"
     args << "--enable-libbs2b" if build.with? "libbs2b"
     args << "--enable-libcaca" if build.with? "libcaca"
-    args << "--enable-libfdk-aac" if build.with? "fdk-aac"
     args << "--enable-libgme" if build.with? "game-music-emu"
     args << "--enable-libgsm" if build.with? "libgsm"
     args << "--enable-libmodplug" if build.with? "libmodplug"
     args << "--enable-libopenh264" if build.with? "openh264"
     args << "--enable-librsvg" if build.with? "librsvg"
-    args << "--enable-librubberband" if build.with? "rubberband"
     args << "--enable-libsrt" if build.with? "srt"
     args << "--enable-libssh" if build.with? "libssh"
-    args << "--enable-libtesseract" if build.with? "tesseract"
     args << "--enable-libtwolame" if build.with? "two-lame"
     args << "--enable-libvidstab" if build.with? "libvidstab"
     args << "--enable-libvmaf" if build.with? "libvmaf"
@@ -131,18 +135,7 @@ class Ffmpeg < Formula
     args << "--enable-libwebp" if build.with? "webp"
     args << "--enable-libzimg" if build.with? "zimg"
     args << "--enable-libzmq" if build.with? "zeromq"
-    args << "--enable-opencl" if MacOS.version > :lion
-    args << "--enable-openssl" if build.with? "openssl"
-
-    if build.with? "openjpeg"
-      args << "--enable-libopenjpeg"
-      args << "--disable-decoder=jpeg2000"
-      args << "--extra-cflags=" + `pkg-config --cflags libopenjp2`.chomp
-    end
-
-    # These librares are GPL-incompatible, and require ffmpeg be built with
-    # the "--enable-nonfree" flag, which produces unredistributable libraries
-    args << "--enable-nonfree" if build.with?("fdk-aac") || build.with?("openssl")
+    args << "--extra-cflags=" + `pkg-config --cflags libopenjp2`.chomp
 
     ENV.O3
     system "./configure", *args
