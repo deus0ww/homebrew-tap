ff8dd79c3fd0d3c6ff01cab5c2f9c82974b907bb
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-06-10 01:20:21 +0700

    Create libmysofa.rb

---
+++ libmysofa.rb
@@ -0,0 +1,24 @@
+class Libmysofa < Formula
+  desc "Library for reading AES SOFA files"
+  homepage "https://github.com/hoene/libmysofa"
+  url "https://github.com/hoene/libmysofa/archive/v0.7.tar.gz"
+  sha256 "c1e6a0a91fee89625a60befec674bf2b4bf17055676933727f106785e0ea42a3"
+  head "https://github.com/hoene/libmysofa.git"
+
+  depends_on "cmake" => :build
+
+  depends_on "cunit"
+
+  def install
+    cd "build" do
+      system "cmake", *std_cmake_args, "-DCMAKE_BUILD_TYPE=Debug", ".."
+      system "make", "all"
+    end
+  end
+
+  test do
+    cd "build" do
+      system "make", "test"
+    end
+  end
+end
