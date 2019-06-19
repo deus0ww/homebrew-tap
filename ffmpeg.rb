a5df3b27b012b7faf8e9635d0c950cc736e7ca84
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-06-19 10:09:59 +0700

    Fixed FFMPEG build
    Removed --enable-hardcoded-tables

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -81,7 +81,6 @@ class Ffmpeg < Formula
       --enable-avresample
       --enable-ffplay
       --enable-gpl
-      --enable-hardcoded-tables
       --enable-nonfree
       --enable-opencl
       --enable-pthreads
