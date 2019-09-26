510389660c5a188b0d1b6d6fcdb00db761c0f2a1
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-09-27 03:48:32 +0700

    Update libass.rb

--- libass.rb
+++ libass.rb
@@ -3,10 +3,9 @@ class Libass < Formula
   homepage "https://github.com/libass/libass"
   url "https://github.com/libass/libass/releases/download/0.14.0/libass-0.14.0.tar.xz"
   sha256 "881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38a2"
-  revision 2
 
   head do
-    url "https://github.com/deus0ww/libass.git"
+    url "https://github.com/libass/libass.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
