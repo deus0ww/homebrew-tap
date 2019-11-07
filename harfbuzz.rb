79a1b81472188c66873f045db713ae5506c80d49
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-11-07 15:46:00 +0700

    Added Harfbuzz

---
+++ harfbuzz.rb
@@ -0,0 +1,60 @@
+class Harfbuzz < Formula
+  desc "OpenType text shaping engine"
+  homepage "https://wiki.freedesktop.org/www/Software/HarfBuzz/"
+  url "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.6.4.tar.xz"
+  sha256 "9413b8d96132d699687ef914ebb8c50440efc87b3f775d25856d7ec347c03c12"
+
+  head do
+    url "https://github.com/behdad/harfbuzz.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "ragel" => :build
+  end
+
+  depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "freetype"
+  depends_on "glib"
+  depends_on "graphite2"
+  depends_on "icu4c"
+
+  resource "ttf" do
+    url "https://github.com/behdad/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
+    sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
+  end
+
+  def install
+    ENV.O3
+    ENV.prepend "CXXFLAGS", "-flto=thin"
+    ENV.prepend "CFLAGS", "-flto=thin"
+    ENV.prepend "LDFLAGS", "-flto=thin"
+  
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-introspection=yes
+      --enable-static
+      --with-cairo=yes
+      --with-coretext=yes
+      --with-freetype=yes
+      --with-glib=yes
+      --with-gobject=yes
+      --with-graphite2=yes
+      --with-icu=yes
+    ]
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    resource("ttf").stage do
+      shape = `echo 'സ്റ്റ്' | #{bin}/hb-shape 270b89df543a7e48e206a2d830c0e10e5265c630.ttf`.chomp
+      assert_equal "[glyph201=0+1183|U0D4D=0+0]", shape
+    end
+  end
+end
