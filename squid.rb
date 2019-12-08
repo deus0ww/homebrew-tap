4e9e5a8058224f134ae025720cb828445cbd303e
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-12-09 04:48:08 +0700

    Create squid.rb

---
+++ squid.rb
@@ -0,0 +1,93 @@
+class Squid < Formula
+  desc "Advanced proxy caching server for HTTP, HTTPS, FTP, and Gopher"
+  homepage "http://www.squid-cache.org/"
+  url "http://www.squid-cache.org/Versions/v4/squid-4.9.tar.xz"
+  sha256 "1cb1838c6683b0568a3a4050f4ea2fc1eaa5cbba6bdf7d57f7258c7cd7b41fa1"
+  head "https://github.com/squid-cache/squid.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  depends_on "openssl@1.1"
+
+  def install
+    ENV.O3
+    ENV.append "CXXFLAGS", "-Ofast -flto=thin -march=native -mtune=native"
+    ENV.append "CFLAGS", "-Ofast -flto=thin -march=native -mtune=native"
+    ENV.append "LDFLAGS", "-Ofast -flto=thin"  
+
+    # https://stackoverflow.com/questions/20910109/building-squid-cache-on-os-x-mavericks
+    ENV.append "LDFLAGS", "-lresolv"
+
+    # For --disable-eui, see:
+    # http://www.squid-cache.org/mail-archive/squid-users/201304/0040.html
+    args = %W[
+      --localstatedir=#{var}
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-eui
+
+      --enable-delay-pools
+      --enable-pf-transparent
+      --enable-ssl
+      --enable-ssl-crtd
+
+      --enable-disk-io
+      --enable-removal-policies
+      --enable-storeio
+
+      --with-included-ltdl
+      --with-openssl
+    ]
+
+    system "./bootstrap.sh" if build.head?
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  plist_options :manual => "squid"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_sbin}/squid</string>
+        <string>-N</string>
+        <string>-d 1</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/squid -v")
+
+    pid = fork do
+      exec "#{sbin}/squid"
+    end
+    sleep 2
+
+    begin
+      system "#{sbin}/squid", "-k", "check"
+    ensure
+      exec "#{sbin}/squid -k interrupt"
+      Process.wait(pid)
+    end
+  end
+end
