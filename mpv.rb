82eeb8ac3c5f1aa6678d2d11a5f62758832f68d1
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-12-30 20:38:40 +0700

    Update mpv.rb

--- mpv.rb
+++ mpv.rb
@@ -1,8 +1,8 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.30.0.tar.gz"
-  sha256 "33a1bcb7e74ff17f070e754c15c52228cf44f2cefbfd8f34886ae81df214ca35"
+  url "https://github.com/mpv-player/mpv/archive/v0.31.0.tar.gz"
+  sha256 "805a3ac8cf51bfdea6087a6480c18835101da0355c8e469b6d488a1e290585a5"
   head "https://github.com/mpv-player/mpv.git"
 
   depends_on "docutils" => :build
