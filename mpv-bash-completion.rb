76f80b100716e1fa580361f8abd672ec52762f23
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-01-06 15:30:49 +0700

    Update mpv-bash-completion.rb

--- mpv-bash-completion.rb
+++ mpv-bash-completion.rb
@@ -1,8 +1,8 @@
 class MpvBashCompletion < Formula
   desc "Bash completion for the mpv video player"
   homepage "https://github.com/2ion/mpv-bash-completion"
-  url "https://github.com/2ion/mpv-bash-completion/archive/3.3.17.tar.gz"
-  sha256 "355f9ae90638c730028e0b6a3ae6e9c09f5cb1ede27372f8a8cf7b0035020e97"
+  url "https://github.com/deus0ww/mpv-bash-completion/archive/3.3.18.tar.gz"
+  sha256 "6ee2652bf44429e426cd6a1055961389795de14759f9ce0741cef95e4bb35f67"
   head "https://github.com/deus0ww/mpv-bash-completion.git"
 
   depends_on "bash"
