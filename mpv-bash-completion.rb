c679b3a2ca28bf437faa8edaeec93f6a4dd4a5b0
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-01-06 14:50:19 +0700

    Switch mpv-bash-completion to fork to use luajit

--- mpv-bash-completion.rb
+++ mpv-bash-completion.rb
@@ -3,12 +3,12 @@ class MpvBashCompletion < Formula
   homepage "https://github.com/2ion/mpv-bash-completion"
   url "https://github.com/2ion/mpv-bash-completion/archive/3.3.17.tar.gz"
   sha256 "355f9ae90638c730028e0b6a3ae6e9c09f5cb1ede27372f8a8cf7b0035020e97"
-  head "https://github.com/2ion/mpv-bash-completion.git"
+  head "https://github.com/deus0ww/mpv-bash-completion.git"
 
   depends_on "bash"
   depends_on "coreutils"
+  depends_on "deus0ww/tap/luajit"
   depends_on "deus0ww/tap/mpv"
-  depends_on "lua"
 
   def install
     system "make", "-f", "Makefile.osx"
