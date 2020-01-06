aeb3c7fdd92ab99d55555e78129af423683b065e
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-01-06 15:56:15 +0700

    Update highlight.rb

--- highlight.rb
+++ highlight.rb
@@ -5,12 +5,6 @@ class Highlight < Formula
   sha256 "8a50a85e94061b53085c6ad8cf110039217dbdd411ab846f9ff934bec7ecd6d0"
   head "https://gitlab.com/saalen/highlight.git"
 
-  bottle do
-    sha256 "c02c61dfc4aee5d36e7567fba45f05552bab5e26a99d7b39693149ade95d3c7f" => :catalina
-    sha256 "a0f9bcef112645b17cfc3b71379a4d0da12c6e1341a3690fb23f3721002db317" => :mojave
-    sha256 "b917103aa86aa45d780a1118e8058daafab9ae749fac1a18cbc8000080056bd7" => :high_sierra
-  end
-
   depends_on "boost" => :build
   depends_on "pkg-config" => :build
   depends_on "luajit"
