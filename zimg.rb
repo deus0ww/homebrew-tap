a7d414643b078bd3417098a5c019446285b9b4e3
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-02-13 03:49:01 +0700

    Update zimg.rb

--- zimg.rb
+++ zimg.rb
@@ -5,14 +5,6 @@ class Zimg < Formula
   sha256 "10403c2964fe11b559a7ec5e081c358348fb787e26b91ec0d1f9dd7c01d1cd7b"
   head "https://github.com/sekrit-twc/zimg.git"
 
-  bottle do
-    cellar :any
-    sha256 "01b1116ebf7b0c065e19a3ce31c92965c361ba964dcc892f6fdbf69d98d94912" => :catalina
-    sha256 "7e22530b80432466153372f3d05580cf64d23d5f4fad6825887dad65737092b3" => :mojave
-    sha256 "699e5d20544252543c021de24b90aa9daae0f455310e030c485cd29b9eb350c5" => :high_sierra
-    sha256 "467810f773bb00aab981b5ad649751b9fe061a555babf15979f08a76b120f3db" => :sierra
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
