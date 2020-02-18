aa50e422decb5ebd4fedea035ce4dcbe90440feb
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-02-18 22:23:14 +0700

    Update from core - removed xcode hack

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -68,10 +68,6 @@ class Ffmpeg < Formula
   depends_on "zeromq" => :optional
 
   def install
-    # Work around Xcode 11 clang bug
-    # https://bitbucket.org/multicoreware/x265/issues/514/wrong-code-generated-on-macos-1015
-    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
-
     args = %W[
       --cc=#{ENV.cc}
       --host-cflags=#{ENV.cflags}
