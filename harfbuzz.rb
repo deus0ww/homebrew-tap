979e91ca8a5e095175400f9ab73a5a0331ada0a6
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-08-04 16:02:10 +0700

    Update harfbuzz.rb

--- harfbuzz.rb
+++ harfbuzz.rb
@@ -1,30 +1,31 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/releases/download/2.6.8/harfbuzz-2.6.8.tar.xz"
-  sha256 "6648a571a27f186e47094121f0095e1b809e918b3037c630c7f38ffad86e3035"
+  url "https://github.com/harfbuzz/harfbuzz/archive/2.7.0.tar.gz"
+  sha256 "4dba05de1fd44705f54c40d801e0e3d4833555d004cb611cc18675173feae75b"
   license "MIT"
+  head "https://github.com/harfbuzz/harfbuzz.git"
 
-  head do
-    url "https://github.com/harfbuzz/harfbuzz.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "ragel" => :build
-  end
-
-  depends_on "gobject-introspection" => :build
-  depends_on "pkg-config" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "cairo"
   depends_on "freetype"
   depends_on "glib"
+  depends_on "gobject-introspection"
   depends_on "graphite2"
   depends_on "icu4c"
 
   resource "ttf" do
     url "https://github.com/harfbuzz/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
     sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
+  end
+
+  # Fix linking issues on High Sierra
+  # https://github.com/harfbuzz/harfbuzz/pull/2605
+  # Remove in next release
+  patch do
+    url "https://github.com/harfbuzz/harfbuzz/commit/7c61caa7384e9c3afa0d9237bf6cd303eb5ef3a1.patch?full_index=1"
+    sha256 "4d60b681918c2b911da9a84e37386ed1fa48794d5c943ff9a7bd50eb3a255969"
   end
 
   def install
@@ -36,24 +37,23 @@ class Harfbuzz < Formula
     ENV.append "OBJCFLAGS",   opts
     ENV.append "OBJCXXFLAGS", opts
     ENV.append "LDFLAGS",     opts + " -dead_strip"
-
-    args = %W[
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --enable-introspection=yes
-      --enable-static
-      --with-cairo=yes
-      --with-coretext=yes
-      --with-freetype=yes
-      --with-glib=yes
-      --with-gobject=yes
-      --with-graphite2=yes
-      --with-icu=yes
+    args = %w[
+      --default-library=both
+      -Dcairo=enabled
+      -Dcoretext=enabled
+      -Dfreetype=enabled
+      -Dglib=enabled
+      -Dgobject=enabled
+      -Dgraphite=enabled
+      -Dicu=enabled
+      -Dintrospection=enabled
     ]
 
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, *args, ".."
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   test do
