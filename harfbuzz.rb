73e90c2c36f2aeb3b5188556750aed345c6862cf
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-08-14 00:15:39 +0700

    Update harfbuzz.rb

--- harfbuzz.rb
+++ harfbuzz.rb
@@ -1,8 +1,8 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/archive/2.7.0.tar.gz"
-  sha256 "4dba05de1fd44705f54c40d801e0e3d4833555d004cb611cc18675173feae75b"
+  url "https://github.com/harfbuzz/harfbuzz/archive/2.7.1.tar.gz"
+  sha256 "431c856ff18eeca89b2a36b58f2c2d56273cd522c34c0ffbc9dd6f7da4b9bd08"
   license "MIT"
   head "https://github.com/harfbuzz/harfbuzz.git"
 
@@ -18,14 +18,6 @@ class Harfbuzz < Formula
   resource "ttf" do
     url "https://github.com/harfbuzz/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
     sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
-  end
-
-  # Fix linking issues on High Sierra
-  # https://github.com/harfbuzz/harfbuzz/pull/2605
-  # Remove in next release
-  patch do
-    url "https://github.com/harfbuzz/harfbuzz/commit/7c61caa7384e9c3afa0d9237bf6cd303eb5ef3a1.patch?full_index=1"
-    sha256 "4d60b681918c2b911da9a84e37386ed1fa48794d5c943ff9a7bd50eb3a255969"
   end
 
   def install
