8d7bb5fb5dcec5cbf9a50d2de693eb36e39a32bb
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-08-25 15:45:23 +0700

    Update squid.rb

--- squid.rb
+++ squid.rb
@@ -1,8 +1,8 @@
 class Squid < Formula
   desc "Advanced proxy caching server for HTTP, HTTPS, FTP, and Gopher"
   homepage "http://www.squid-cache.org/"
-  url "http://www.squid-cache.org/Versions/v4/squid-4.12.tar.xz"
-  sha256 "f42a03c8b3dc020722c88bf1a87da8cb0c087b2f66b41d8256c77ee1b527e317"
+  url "http://www.squid-cache.org/Versions/v4/squid-4.13.tar.xz"
+  sha256 "6891a0f540e60779b4f24f1802a302f813c6f473ec7336a474ed68c3e2e53ee0"
   license "GPL-2.0"
 
   head do
