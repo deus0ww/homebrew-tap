304ed64129c022610606dee9cda717645f9f2797
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-11-17 00:23:31 +0700

    Update highlight.rb

--- highlight.rb
+++ highlight.rb
@@ -3,8 +3,8 @@
 class Highlight < Formula
   desc "Convert source code to formatted text with syntax highlighting"
   homepage "http://www.andre-simon.de/doku/highlight/en/highlight.php"
-  url "http://www.andre-simon.de/zip/highlight-3.58.tar.bz2"
-  sha256 "df80251be1f83adfc58aaad589fd9a8f9a3866b0d89b9f3c81b1696f07db45f8"
+  url "http://www.andre-simon.de/zip/highlight-3.59.tar.bz2"
+  sha256 "85926ca8e08e69b497fa4a0c93bec595e15c1ca18c4ee494ea3f1c9a78c249a2"
   license "GPL-3.0-or-later"
   head "https://gitlab.com/saalen/highlight.git"
 
