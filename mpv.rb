9bbb679f016607953b733de8f4b53598edead0cc
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-11-24 15:03:35 +0700

    Update mpv.rb

--- mpv.rb
+++ mpv.rb
@@ -3,8 +3,8 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.32.0.tar.gz"
-  sha256 "9163f64832226d22e24bbc4874ebd6ac02372cd717bef15c28a0aa858c5fe592"
+  url "https://github.com/mpv-player/mpv/archive/v0.33.0.tar.gz"
+  sha256 "f1b9baf5dc2eeaf376597c28a6281facf6ed98ff3d567e3955c95bf2459520b4"
   license :cannot_represent
   head "https://github.com/mpv-player/mpv.git" # , branch: "name"
 
