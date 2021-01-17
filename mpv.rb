e59e7172f349360600289391c4f42665ca29df9a
deus0ww <6702585+deus0ww@users.noreply.github.com>
2021-01-18 03:59:30 +0700

    Removed unneeded formulas

--- mpv.rb
+++ mpv.rb
@@ -15,11 +15,11 @@ class Mpv < Formula
 
   depends_on "deus0ww/tap/ffmpeg"
   depends_on "deus0ww/tap/libass"
-  depends_on "deus0ww/tap/little-cms2"
   depends_on "deus0ww/tap/luajit"
 
   depends_on "jpeg"
   depends_on "libarchive"
+  depends_on "little-cms2"
   depends_on "mujs"
   depends_on "rubberband"
   depends_on "uchardet"
