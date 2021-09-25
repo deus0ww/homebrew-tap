48c2487bd1ca58309a978b8562a9561a9c58da42
deus0ww <6702585+deus0ww@users.noreply.github.com>
2021-09-25 18:48:01 +0700

    yt-dlp - Use formula from official tap

--- yt-dlp.rb
+++ yt-dlp.rb
@@ -1,18 +1,22 @@
 class YtDlp < Formula
   include Language::Python::Virtualenv
 
-  desc "Download YouTube videos from the command-line"
+  desc "A youtube-dl fork with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://github.com/yt-dlp/yt-dlp/archive/refs/tags/2021.04.22.tar.gz"
-  sha256 "0e0db3ae4a876b8012e44d09d14f551fa7604a0cf3ad77ee0e691b2dc5cbdca1"
+  url "https://files.pythonhosted.org/packages/ad/2a/19788cdbce56ea05600068bf342f91c91fd5acc6c6486e16d498b0ec533a/yt-dlp-2021.9.25.tar.gz"
+  sha256 "e7b8dd0ee9498abbd80eb38d9753696d6ca3d02f64980322ab3bf39ba1bc31ee"
   license "Unlicense"
-  head "https://github.com/yt-dlp/yt-dlp.git"
+
+  bottle :unneeded
 
   depends_on "python@3.9"
   depends_on "aria2" => :recommended
 
   def install
     virtualenv_install_with_resources
+    bash_completion.install "completions/bash/yt-dlp"
+    fish_completion.install "completions/fish/yt-dlp.fish"
+    zsh_completion.install "completions/zsh/_yt-dlp"
   end
 
   test do
