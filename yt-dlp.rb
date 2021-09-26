6a76eec8074525792d0713031ac5f43b098b7dcb
deus0ww <6702585+deus0ww@users.noreply.github.com>
2021-09-26 12:11:20 +0700

    yt-dlp - Fixed building HEAD

--- yt-dlp.rb
+++ yt-dlp.rb
@@ -1,27 +1,48 @@
 class YtDlp < Formula
   include Language::Python::Virtualenv
 
-  desc "Youtube-dl fork with additional features and fixes"
+  desc "Fork of youtube-dl with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
   url "https://files.pythonhosted.org/packages/ad/2a/19788cdbce56ea05600068bf342f91c91fd5acc6c6486e16d498b0ec533a/yt-dlp-2021.9.25.tar.gz"
   sha256 "e7b8dd0ee9498abbd80eb38d9753696d6ca3d02f64980322ab3bf39ba1bc31ee"
   license "Unlicense"
 
+  head do
+    url "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
+    depends_on "pandoc" => :build
+  end
 
   depends_on "python@3.9"
   depends_on "aria2" => :recommended
 
+  resource "mutagen" do
+    url "https://files.pythonhosted.org/packages/f3/d9/2232a4cb9a98e2d2501f7e58d193bc49c956ef23756d7423ba1bd87e386d/mutagen-1.45.1.tar.gz"
+    sha256 "6397602efb3c2d7baebd2166ed85731ae1c1d475abca22090b7141ff5034b3e1"
+  end
+
+  resource "pycryptodome" do
+    url "https://files.pythonhosted.org/packages/f8/8e/14a8238190bcf1bab3d58432cd795c859edbc2f5abd8460f80438046a799/pycryptodome-3.10.4.tar.gz"
+    sha256 "40083b0d7f277452c7f2dd4841801f058cc12a74c219ee4110d65774c6a58bef"
+  end
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/1c/f4/61aee1eb4baadf8477fb7f3bc6b04a50fe683ef8ad2f60282806821e4b3b/websockets-10.0.tar.gz"
+    sha256 "c4fc9a1d242317892590abe5b61a9127f1a61740477bfb121743f290b8054002"
+  end
+
   def install
+    system "make" if build.head?
     virtualenv_install_with_resources
+    man1.install_symlink libexec/"share/man/man1/yt-dlp.1"
     bash_completion.install "completions/bash/yt-dlp"
-    fish_completion.install "completions/fish/yt-dlp.fish"
     zsh_completion.install "completions/zsh/_yt-dlp"
+    fish_completion.install "completions/fish/yt-dlp.fish"
   end
 
   test do
-    # commit history of homebrew-core repo
+    # "History of homebrew-core", uploaded 3 Feb 2020
     system "#{bin}/yt-dlp", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
-    # homebrew playlist
+    # "homebrew", playlist last updated 3 Mar 2020
     system "#{bin}/yt-dlp", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
   end
 end
