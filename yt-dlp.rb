643ba324408855fdb5aa4e0fc534b345c6a87585
deus0ww <6702585+deus0ww@users.noreply.github.com>
2021-12-27 17:29:18 +0700

    Update yt-dlp.rb

--- yt-dlp.rb
+++ yt-dlp.rb
@@ -1,7 +1,7 @@
 class YtDlp < Formula
   include Language::Python::Virtualenv
 
-  desc "A youtube-dl fork with additional features and fixes"
+  desc "Fork of youtube-dl with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
   url "https://files.pythonhosted.org/packages/e0/c5/1748e553776f00057fb4b0f648eb22b7e0d58a3c062d2d4a6b8aba72fb4e/yt-dlp-2021.12.27.tar.gz"
   sha256 "2244df3759751487e796b23b67216bee98e70832a3a43c2526b0b0e0bbfbcb5b"
@@ -37,9 +37,9 @@ class YtDlp < Formula
   end
 
   test do
-    # commit history of homebrew-core repo
+    # "History of homebrew-core", uploaded 3 Feb 2020
     system "#{bin}/yt-dlp", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
-    # homebrew playlist
+    # "homebrew", playlist last updated 3 Mar 2020
     system "#{bin}/yt-dlp", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
   end
 end
