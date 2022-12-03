a3fe275210bf05e09ccc5eb1359e2ebc56ad8556
deus0ww <6702585+deus0ww@users.noreply.github.com>
2022-12-04 05:55:13 +0700

    Update libplacebo.rb

--- libplacebo.rb
+++ libplacebo.rb
@@ -3,7 +3,7 @@ class Libplacebo < Formula
 
   desc "Reusable library for GPU-accelerated image/video processing primitives"
   homepage "https://code.videolan.org/videolan/libplacebo"
-  url "https://code.videolan.org/videolan/libplacebo/-/archive/v4.208.0/libplacebo-v4.208.0.tar.bz2"
+  url "https://code.videolan.org/videolan/libplacebo/-/archive/v5.229.1/libplacebo-v5.229.1.tar.bz2"
   sha256 "c89a80655ab375e4809415bb597c638607fc150fa6f6bb830dd502fec7f0ba95"
   license "LGPL-2.1-or-later"
   head "https://code.videolan.org/videolan/libplacebo.git", branch: "master"
