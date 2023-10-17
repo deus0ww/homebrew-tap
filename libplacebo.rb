0d9f0db658a4fdabd2c1a1d5cf1bb0ede194b042
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2023-10-17 15:31:35 +0700

    libplacebo - added shaderc

--- libplacebo.rb
+++ libplacebo.rb
@@ -18,14 +18,20 @@ class Libplacebo < Formula
 
   depends_on "little-cms2"
   depends_on "sdl2"
+  depends_on "shaderc"
   depends_on "vulkan-loader"
-
-  resource "glad" do
+  
+  resource "fast_float" do
+    url "https://github.com/fastfloat/fast_float/archive/refs/tags/v5.2.0.tar.gz"
+    sha256 "72bbfd1914e414c920e39abdc81378adf910a622b62c45b4c61d344039425d18"
+  end
+    
+  resource "glad2" do
     url "https://files.pythonhosted.org/packages/8b/b3/191508033476b6a409c070c6166b1c41ebb547cc6136260e9157343e6a2b/glad2-2.0.4.tar.gz"
     sha256 "ede1639f69f2ba08f1f498a40a707f34a609d24eb2ea0d6c9364689a798cf7d0"
   end
 
-  resource "jinja" do
+  resource "jinja2" do
     url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
     sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
   end
@@ -37,13 +43,26 @@ class Libplacebo < Formula
 
   def install
     resources.each do |r|
-      r.stage(Pathname("3rdparty")/r.name)
+      # Override resource name to use expected directory name
+      dir_name = case r.name
+      when "glad2", "jinja2"
+        r.name.sub(/\d+$/, "")
+      else
+        r.name
+      end
+
+      r.stage(Pathname("3rdparty")/dir_name)
     end
 
     args = %w[
       -Db_lto=true
       -Db_lto_mode=thin
       -Ddemos=false
+
+      -Dvulkan=enabled
+      -Dopengl=enabled
+      -Dshaderc=enabled
+      -Dlcms=enabled
     ]
     args << ("-Dc_args=" + (Hardware::CPU.arm? ? "-mcpu=native" : "-march=native -mtune=native") + " -Ofast")
 
