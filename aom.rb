f337c0f3a615808cc6582e7a7f38b7be0f1e2cf3
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2023-10-31 15:19:24 +0700

    Updated aom and jpeg-xl from homebrew core

--- aom.rb
+++ aom.rb
@@ -2,18 +2,14 @@ class Aom < Formula
   desc "Codec library for encoding and decoding AV1 video streams"
   homepage "https://aomedia.googlesource.com/aom"
   url "https://aomedia.googlesource.com/aom.git",
-      tag:      "v3.6.1",
-      revision: "7ade96172b95adc91a5d85bf80c90989cd543ee8"
+      tag:      "v3.7.0",
+      revision: "6054fae218eda6e53e1e3b4f7ef0fff4877c7bf1"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
-
-  # `jpeg-xl` is currently not bottled on Linux
-  on_macos do
-    depends_on "pkg-config" => :build
-    depends_on "deus0ww/tap/jpeg-xl"
-    depends_on "libvmaf" => :optional
-  end
+  depends_on "pkg-config" => :build
+  depends_on "jpeg-xl"
+  depends_on "libvmaf":optional
 
   on_intel do
     depends_on "yasm" => :build
@@ -24,10 +20,14 @@ class Aom < Formula
     sha256 "868fc3446d37d0c6959a48b68906486bd64788b2e795f0e29613cbb1fa73480e"
   end
 
-  def install
-    ENV.runtime_cpu_detection unless Hardware::CPU.arm?
+  # Fix build on arm64 macOS.
+  # https://aomedia-review.googlesource.com/c/aom/+/180942
+  patch :DATA
 
-    args = std_cmake_args + [
+  def install
+    ENV.runtime_cpu_detection
+
+    args = [
       "-DCMAKE_INSTALL_RPATH=#{rpath}",
       "-DENABLE_DOCS=off",
       "-DENABLE_EXAMPLES=on",
@@ -36,18 +36,10 @@ class Aom < Formula
       "-DENABLE_TOOLS=off",
       "-DBUILD_SHARED_LIBS=on",
     ]
-    # Runtime CPU detection is not currently enabled for ARM on macOS.
-    args << "-DCONFIG_RUNTIME_CPU_DETECT=0" if Hardware::CPU.arm?
+    args << "-DCONFIG_TUNE_BUTTERAUGLI=1" if build.with? "libvmaf"
+    args << "-DCONFIG_TUNE_VMAF=1"        if build.with? "libvmaf"
 
-    # Make unconditional when `jpeg-xl` is bottled on Linux
-    if OS.mac? and build.with? "libvmaf"
-      args += [
-        # "-DCONFIG_TUNE_BUTTERAUGLI=1",
-        "-DCONFIG_TUNE_VMAF=1",
-      ]
-    end
-
-    system "cmake", "-S", ".", "-B", "brewbuild", *args
+    system "cmake", "-S", ".", "-B", "brewbuild", *std_cmake_args, *args
     system "cmake", "--build", "brewbuild"
     system "cmake", "--install", "brewbuild"
   end
@@ -66,3 +58,20 @@ class Aom < Formula
     end
   end
 end
+
+__END__
+diff --git a/build/cmake/aom_configure.cmake b/build/cmake/aom_configure.cmake
+index 6c932e86c8..917e7cac5d 100644
+--- a/build/cmake/aom_configure.cmake
++++ b/build/cmake/aom_configure.cmake
+@@ -184,7 +184,9 @@ if(AOM_TARGET_CPU STREQUAL "x86" OR AOM_TARGET_CPU STREQUAL "x86_64")
+   string(STRIP "${AOM_AS_FLAGS}" AOM_AS_FLAGS)
+ elseif(AOM_TARGET_CPU MATCHES "arm")
+   if(AOM_TARGET_SYSTEM STREQUAL "Darwin")
+-    set(CMAKE_ASM_COMPILER as)
++    if(NOT CMAKE_ASM_COMPILER)
++      set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
++    endif()
+     set(AOM_AS_FLAGS -arch ${AOM_TARGET_CPU} -isysroot ${CMAKE_OSX_SYSROOT})
+   elseif(AOM_TARGET_SYSTEM STREQUAL "Windows")
+     if(NOT CMAKE_ASM_COMPILER)
--- jpeg-xl.rb
+++ jpeg-xl.rb
@@ -1,7 +1,7 @@
 class JpegXl < Formula
   desc "New file format for still image compression"
   homepage "https://jpeg.org/jpegxl/index.html"
-  url "https://github.com/libjxl/libjxl/archive/v0.8.2.tar.gz"
+  url "https://github.com/libjxl/libjxl/archive/refs/tags/v0.8.2.tar.gz"
   sha256 "c70916fb3ed43784eb840f82f05d390053a558e2da106e40863919238fa7b420"
   license "BSD-3-Clause"
 
@@ -37,7 +37,7 @@ class JpegXl < Formula
   # https://github.com/libjxl/libjxl/tree/v#{version}/third_party
   resource "sjpeg" do
     url "https://github.com/webmproject/sjpeg.git",
-        revision: "e5ab13008bb214deb66d5f3e17ca2f8dbff150bf"
+        revision: "868ab558fad70fcbe8863ba4e85179eeb81cc840"
   end
 
   def install
@@ -52,7 +52,8 @@ class JpegXl < Formula
                     "-DJPEGXL_VERSION=#{version}",
                     "-DJPEGXL_ENABLE_MANPAGES=OFF",
                     "-DCMAKE_INSTALL_RPATH=#{rpath}",
-                    # "-DPython3_EXECUTABLE=#{Formula["asciidoc"].libexec/"bin/python3"}",
+                    #"-DPython_EXECUTABLE=#{Formula["asciidoc"].libexec/"bin/python"}",
+                    #"-DPython3_EXECUTABLE=#{Formula["asciidoc"].libexec/"bin/python3"}",
                     "-DCMAKE_CXX_FLAGS=-DFJXL_ENABLE_AVX512=0",
                     *std_cmake_args
     system "cmake", "--build", "build"
