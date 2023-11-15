8b73c6ecf83d23b6b4e327e94f18d4129bbdff2f
deus0ww <6702585+deus0ww@users.noreply.github.com>
2023-11-16 00:20:25 +0700

    Updated dockutil from homebrew-core

--- dockutil@3.rb
+++ dockutil@3.rb
@@ -1,30 +1,37 @@
 class DockutilAT3 < Formula
   desc "Tool for managing dock items"
   homepage "https://github.com/kcrawford/dockutil"
-  url "https://github.com/kcrawford/dockutil/archive/3.0.2.tar.gz"
+  url "https://github.com/kcrawford/dockutil/archive/refs/tags/3.0.2.tar.gz"
   sha256 "8d0117fccd836a14782107bdd3619df17dd4708470e3522f5b2ab0456769ba79"
   license "Apache-2.0"
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  head do
+    url "https://github.com/kcrawford/dockutil.git", branch: "main"
+
+    depends_on xcode: ["13.0", :build]
+
+    uses_from_macos "swift"
   end
 
-  depends_on xcode: ["12.5", :build]
   depends_on :macos
 
   # Create Package.swift
-  patch :DATA
+  patch :DATA unless build.head?
 
   def install
-    system "swift", "build", "--disable-sandbox", "--configuration", "release"
-    bin.install ".build/release/dockutil"
+    if build.head?
+      system "swift", "build", "--disable-sandbox", "--configuration", "release"
+      bin.install ".build/release/dockutil"
+    else
+      bin.install "scripts/dockutil"
+    end
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/dockutil --version")
   end
 end
+
 __END__
 diff -urN a/Package.swift b/Package.swift
 --- a/Package.swift	1969-12-31 19:00:00.000000000 -0500
