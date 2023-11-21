65e1790e3395e577819eb6791c7702048ea4d156
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2023-11-21 12:15:51 +0700

    Change only shader cache size

--- mpv.rb
+++ mpv.rb
@@ -14,7 +14,7 @@ class Mpv < Formula
       end
         patch do # Set shader/icc caches to 150MB each
           url "https://github.com/deus0ww/homebrew-tap/raw/master/mpv-bigger-gpu-cache.patch"
-          sha256 "6a5bab845856250b6e8c5c2532d95d3788c0e1a224d9aba53f8f58acb7dd70d7"
+          sha256 "414dcbedea1c64a5e4a5f7e562d06392291eb49787b4d718a26e8a487b9a53a9"
         end
     end
   else
@@ -34,7 +34,7 @@ class Mpv < Formula
         end
         patch do # Set shader/icc caches to 150MB each
           url "https://github.com/deus0ww/homebrew-tap/raw/master/mpv-bigger-gpu-cache.patch"
-          sha256 "6a5bab845856250b6e8c5c2532d95d3788c0e1a224d9aba53f8f58acb7dd70d7"
+          sha256 "414dcbedea1c64a5e4a5f7e562d06392291eb49787b4d718a26e8a487b9a53a9"
         end
       else
         # Last buildable commit on macOS 10.13 - 0.36.0-722-g7480efa62c
