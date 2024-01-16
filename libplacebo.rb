cc1f755cbb6a5d9596fc7f604298965780d60516
deus0ww <6702585+deus0ww@users.noreply.github.com>
2024-01-16 17:56:40 +0700

    Update libplacebo.rb

--- libplacebo.rb
+++ libplacebo.rb
@@ -58,8 +58,8 @@ class Libplacebo < Formula
   depends_on "xxhash"
 
   resource "fast_float" do
-    url "https://github.com/fastfloat/fast_float/archive/refs/tags/v5.3.0.tar.gz"
-    sha256 "2f3bc50670455534dcaedc9dcd0517b71152f319d0cec8625f21c51d23eaf4b9"
+    url "https://github.com/fastfloat/fast_float/archive/refs/tags/v6.0.0.tar.gz"
+    sha256 "7e98671ef4cc7ed7f44b3b13f80156c8d2d9244fac55deace28bd05b0a2c7c8e"
   end
 
   resource "glad2" do
@@ -68,8 +68,8 @@ class Libplacebo < Formula
   end
 
   resource "jinja2" do
-    url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
-    sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+    url "https://files.pythonhosted.org/packages/b2/5e/3a21abf3cd467d7876045335e681d276ac32492febe6d98ad89562d1a7e1/Jinja2-3.1.3.tar.gz"
+    sha256 "ac8bd6544d4bb2c9792bf3a159e80bba8fda7f07e81bc3aed565432d5925ba90"
   end
 
   def install
