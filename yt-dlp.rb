81e5d52da42e65301faad29123832142a2daf10f
deus0ww <6702585+deus0ww@users.noreply.github.com>
2024-03-08 16:58:59 +0700

    Update yt-dlp.rb

--- yt-dlp.rb
+++ yt-dlp.rb
@@ -6,21 +6,51 @@ class YtDlp < Formula
   url "https://files.pythonhosted.org/packages/a9/a7/d8536993aed7569c5221f532e3ba01b09d5bdc893df3ef4e5b05d01582c4/yt-dlp-2023.12.30.tar.gz"
   sha256 "a11862e57721b0a0f0883dfeb5a4d79ba213a2d4c45e1880e9fd70f8e6570c38"
   license "Unlicense"
-  head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
 
-  depends_on "make" => :build
-  depends_on "python-brotli"
-  depends_on "python-certifi"
-  depends_on "python-charset-normalizer"
-  depends_on "python-idna"
-  depends_on "python-mutagen"
-  depends_on "python-requests"
-  depends_on "python-urllib3"
+  head do
+    url "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
+
+    on_macos do
+      depends_on "make" => :build
+    end
+  end
+
+  depends_on "certifi"
   depends_on "python@3.12"
+
+  resource "brotli" do
+    url "https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b/Brotli-1.1.0.tar.gz"
+    sha256 "81de08ac11bcb85841e440c13611c00b67d3bf82698314928d0b676362546724"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
+    sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+  end
+
+  resource "mutagen" do
+    url "https://files.pythonhosted.org/packages/81/e6/64bc71b74eef4b68e61eb921dcf72dabd9e4ec4af1e11891bbd312ccbb77/mutagen-1.47.0.tar.gz"
+    sha256 "719fadef0a978c31b4cf3c956261b3c58b6948b32023078a2117b1de09f0fc99"
+  end
 
   resource "pycryptodomex" do
     url "https://files.pythonhosted.org/packages/31/a4/b03a16637574312c1b54c55aedeed8a4cb7d101d44058d46a0e5706c63e1/pycryptodomex-3.20.0.tar.gz"
     sha256 "7a710b79baddd65b806402e14766c721aee8fb83381769c27920f26476276c1e"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
+    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
   end
 
   resource "websockets" do
@@ -30,9 +60,8 @@ class YtDlp < Formula
 
   def install
     if build.head?
-      python3 = "python3.12"
-      system python3, "devscripts/update-version.py"
-      system python3, "devscripts/make_lazy_extractors.py"
+      system "python3.12", "devscripts/update-version.py"
+      system "python3.12", "devscripts/make_lazy_extractors.py"
       system "gmake", "lazy-extractors", "yt-dlp", "completions"
     end
     virtualenv_install_with_resources
@@ -43,9 +72,7 @@ class YtDlp < Formula
   end
 
   test do
-    # "History of homebrew-core", uploaded 3 Feb 2020
     system "#{bin}/yt-dlp", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
-    # "homebrew", playlist last updated 3 Mar 2020
     system "#{bin}/yt-dlp", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
   end
 end
