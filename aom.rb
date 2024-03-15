08c6ee7d40bafbf47e16a1682c8b2ae63fcc8877
deus0ww <6702585+deus0ww@users.noreply.github.com>
2024-03-15 09:54:38 +0700

    Update aom.rb

--- aom.rb
+++ aom.rb
@@ -2,8 +2,8 @@ class Aom < Formula
   desc "Codec library for encoding and decoding AV1 video streams"
   homepage "https://aomedia.googlesource.com/aom"
   url "https://aomedia.googlesource.com/aom.git",
-      tag:      "v3.8.1",
-      revision: "bb6430482199eaefbeaaa396600935082bc43f66"
+      tag:      "v3.8.2",
+      revision: "615b5f541e4434aebd993036bc97ebc1a77ebc25"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
@@ -13,11 +13,6 @@ class Aom < Formula
 
   on_intel do
     depends_on "yasm" => :build
-  end
-
-  resource "homebrew-bus_qcif_15fps.y4m" do
-    url "https://media.xiph.org/video/derf/y4m/bus_qcif_15fps.y4m"
-    sha256 "868fc3446d37d0c6959a48b68906486bd64788b2e795f0e29613cbb1fa73480e"
   end
 
   def install
@@ -41,16 +36,21 @@ class Aom < Formula
   end
 
   test do
-    resource("homebrew-bus_qcif_15fps.y4m").stage do
-      system "#{bin}/aomenc", "--webm",
-                              "--tile-columns=2",
-                              "--tile-rows=2",
-                              "--cpu-used=8",
-                              "--output=bus_qcif_15fps.webm",
-                              "bus_qcif_15fps.y4m"
-
-      system "#{bin}/aomdec", "--output=bus_qcif_15fps_decode.y4m",
-                              "bus_qcif_15fps.webm"
+    resource "homebrew-bus_qcif_15fps.y4m" do
+      url "https://media.xiph.org/video/derf/y4m/bus_qcif_15fps.y4m"
+      sha256 "868fc3446d37d0c6959a48b68906486bd64788b2e795f0e29613cbb1fa73480e"
     end
+
+    testpath.install resource("homebrew-bus_qcif_15fps.y4m")
+
+    system bin/"aomenc", "--webm",
+                            "--tile-columns=2",
+                            "--tile-rows=2",
+                            "--cpu-used=8",
+                            "--output=bus_qcif_15fps.webm",
+                            "bus_qcif_15fps.y4m"
+
+    system bin/"aomdec", "--output=bus_qcif_15fps_decode.y4m",
+                            "bus_qcif_15fps.webm"
   end
 end
