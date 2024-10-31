c69cfb2ef59e2c2dc9373eb91e63901b81acfee1
deus0ww <6702585+deus0ww@users.noreply.github.com>
2024-10-31 11:16:05 +0700

    Update libmysofa.rb

--- libmysofa.rb
+++ libmysofa.rb
@@ -1,8 +1,8 @@
 class Libmysofa < Formula
   desc "Library for reading AES SOFA files"
   homepage "https://github.com/hoene/libmysofa"
-  url "https://github.com/hoene/libmysofa/archive/refs/tags/v1.3.2.tar.gz"
-  sha256 "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76"
+  url "https://github.com/hoene/libmysofa/archive/refs/tags/v1.3.3.tar.gz"
+  sha256 "a15f7236a2b492f8d8da69f6c71b5bde1ef1bac0ef428b94dfca1cabcb24c84f"
   license "BSD-3-Clause"
   head "https://github.com/hoene/libmysofa.git", branch: "main"
 
