f44bdab4a65a172e1098257abf83d3c4c68d0922
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2025-10-09 22:35:12 +0700

    Update ffmpeg.rb

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -1,8 +1,8 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-8.0.tar.xz"
-  sha256 "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
+  url "https://www.ffmpeg.org/releases/ffmpeg-8.0.tar.bz2"
+  sha256 "3e74acc48ddb9f5f70b6747d3f439d51e7cc5497f097d58e5975c84488f4d186"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
