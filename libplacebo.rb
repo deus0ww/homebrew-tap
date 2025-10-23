2effb1a34c97bb306af12e7599f10b096ff3794f
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2025-10-23 08:32:10 +0700

    Use python@3.14

--- libplacebo.rb
+++ libplacebo.rb
@@ -44,7 +44,7 @@ class Libplacebo < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "python@3.13" => :build
+  depends_on "python@3.14" => :build
   depends_on "vulkan-headers" => :build
 
   depends_on "deus0ww/tap/dovi_tool"
--- mpv.rb
+++ mpv.rb
@@ -25,7 +25,7 @@ class Mpv < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => [:build, :test]
-  depends_on "python@3.13" => :build
+  depends_on "python@3.14" => :build
   depends_on xcode: :build
 
   depends_on "deus0ww/tap/ffmpeg"
@@ -129,7 +129,7 @@ class Mpv < Formula
     zsh_completion.install "etc/_mpv.zsh" => "_mpv"
 
     # Build App Bundle
-    system "python3.13", "TOOLS/osxbundle.py", "build/mpv", "--skip-deps"
+    system "python3.14", "TOOLS/osxbundle.py", "build/mpv", "--skip-deps"
     prefix.install "build/mpv.app"
   end
 
